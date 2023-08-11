38ab1d9bf9f451880380df025ba1f9da870011e0
Nathan Pereira <68254838+Nathan-P2@users.noreply.github.com>
2023-08-11 14:50:49 -0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    var array = a.sort((a, b) => a - b);
+    
+    let unique = a.filter(function(value){
+        return a.indexOf(value) === a.lastIndexOf(value)
+    })
+    
+    return unique;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
